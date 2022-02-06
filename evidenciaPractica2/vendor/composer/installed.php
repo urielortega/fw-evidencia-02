60c859f597b1b59c9c0c2f81ae3cbaa20e9db236
Uriel Ortega <56855947+urielortega@users.noreply.github.com>
2022-02-06 15:55:52 -0600

    Archivos para PHP Password Library
    Agregados usando Composer

---
+++ evidenciaPractica2/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'rych/phpass' => array(
+            'pretty_version' => 'v3.0.0beta1',
+            'version' => '3.0.0.0-beta1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rych/phpass',
+            'aliases' => array(),
+            'reference' => 'f94d30f6ea9ead98ea9497818cbbb28db730ea7d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
